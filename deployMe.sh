ead57bb67d6b8d3fb13dd3dda0524d65cf642bf4
Cyprien Mvuanda <cyprien.mvuanda@turner.com>
2015-11-19 16:59:42 -0500

    ommitting postgres version and creating password

--- deployMe.sh
+++ deployMe.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+# export variables
+export USER="gforge"
+export PASS="gfore"
+export DB="gforge"
+
 echo user_allow_other >> fuse.conf
 mv fuse.conf /etc/fuse.conf
 chmod 644 /etc/fuse.conf
-yum -y install postgresql91 postgresql91-server postgresql91-contrib
-service postgresql-9.1 initdb
-chkconfig postgresql-9.1 on
-echo "local all all trust" >> ~postgres/9.1/data/pg_hba.conf
+yum -y install postgresql postgresql-server postgresql-contrib
+service postgresql initdb
+chkconfig postgresql on
+echo "local all all trust" >> ~postgres/data/pg_hba.conf
 
 # Add only authorized hosts i.e DOME, Rest Services preferably via  config file
-echo host all gforge 172.0.0.0/8 trust >> ~postgres/9.1/data/pg_hba.conf
-echo "listen_addresses = '*'" >> ~postgres/9.1/data/postgresql.conf
-service postgresql-9.1 start
+echo host all gforge 172.0.0.0/8 trust >> ~postgres/data/pg_hba.conf
+echo "listen_addresses = '*'" >> ~postgres/data/postgresql.conf
+service postgresql start
 sleep 30
 
 # create users, prefereably via the same config file above
@@ -19,7 +24,7 @@ su - postgres
 
 # git pull from master DB to get latest version of gforge.psql
 mv gforge.psql /var/lib/pgsql/.
-psql -c 'CREATE ROLE gforge NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;' template1
+psql -c "CREATE ROLE gforge NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN PASSWORD 'test';" template1
 psql -c 'CREATE DATABASE gforge WITH OWNER gforge;' template1
 #psql -c 'createuser -h localhost –p 5432 -S -d -R -l gforge;' template1
 #psql -c 'createdb -h localhost –p 5432 gforge;' template1
