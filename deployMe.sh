e4bd7114a344711557c19b04f926f3a15308fae3
jerome_francis <jerome.francis@ge.com>
2015-11-23 22:08:01 +0000

    deployMe.sh edited online with Bitbucket -- upgrade psql to 9.4

--- deployMe.sh
+++ deployMe.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
 # variables (should come from terraform)
-user='gforge'
-pass='gforge'
-db='gforge'
+#user='gforge'
+#pass='gforge'
+#db='gforge'
 
 # export variables
-echo "export USER=$user" >> ~/.bashrc
+sudo -i # switch to root
+cd ~
+echo "export PSQLUSER=$psqluser" >> ~/.bashrc
 echo "export PASS=$pass" >> ~/.bashrc
 echo "export DB=$db" >> ~/.bashrc
 source ~/.bashrc
 
-echo "USER SET: $USER"
-echo "PASSWORD SET: $pass"
-echo "DB SET: $db"
+echo "USER SET: $PSQLUSER"
+echo "PASSWORD SET: $PASS"
+echo "DB SET: $DB"
 
 echo user_allow_other >> fuse.conf
 mv fuse.conf /etc/fuse.conf
 chmod 644 /etc/fuse.conf
-yum -y install postgresql91 postgresql91-server postgresql91-contrib
-service postgresql-9.1 initdb
-chkconfig postgresql-9.1 on
+yum -y install postgresql94.x86_64 postgresql94-server.x86_64 postgresql94-contrib.x86_64
+service postgresql94 initdb
+chkconfig postgresql94 on
 echo "local all all trust" >> ~postgres/9.1/data/pg_hba.conf
 
 # Add only authorized hosts i.e DOME, Rest Services preferably via  config file
-echo host all gforge 172.0.0.0/8 trust >> ~postgres/9.1/data/pg_hba.conf
-echo "listen_addresses = '*'" >> ~postgres/9.1/data/postgresql.conf
-service postgresql-9.1 start
+echo "host all gforge 172.0.0.0/8 trust" >> ~postgres/data/pg_hba.conf
+echo "listen_addresses = '*'" >> ~postgres/data/postgresql.conf
+service postgresql94 start
 sleep 30
 
 # create users, prefereably via the same config file above
-su - postgres
-
+su postgres
+cd ~postgres
+git clone https://bitbucket.org/DigitalMfgCommons/dmcdb.git
+cd dmcdb
 # git pull from master DB to get latest version of gforge.psql
-mv gforge.psql /var/lib/pgsql/.
-psql -c "CREATE ROLE $user NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN PASSWORD $pass;" template1
+#mv gforge.psql /var/lib/pgsql/.
+[ -z "$PASS" ]
+
+if env | grep -q ^PASS=
+then
+  psql -c "CREATE ROLE $user NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN PASSWORD $pass;" template1
+else
+  psql -c "CREATE ROLE $user NOSUPERUSER NOCREATEDB NOCREATEROLE;" template1
+fi
 psql -c "CREATE DATABASE $db WITH OWNER gforge;" template1
 #psql -c 'createuser -h localhost –p 5432 -S -d -R -l gforge;' template1
 #psql -c 'createdb -h localhost –p 5432 gforge;' template1
