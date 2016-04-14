9d4a6239c12940a8f4e8b8a3a8d5dcb420f43e7f
graichen@ge.com <graichen@ge.com>
2016-04-13 22:54:41 -0400

    helpful debugging queries for IsDMDII_Member

---
+++ testQueries/testQueryIsDMDII_Member.sql
@@ -0,0 +1,6 @@
+﻿select * from organization_dmdii_member dmdii, organization_user orgu, users 
+where 
+users.user_name = 'joeengineer'
+and users.user_id=orgu.user_id 
+and orgu.organization_id = dmdii.organization_id 
+and dmdii.expire_date >= now() 
