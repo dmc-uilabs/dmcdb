b8022b53f4afb77931c1db2841119e32fb1bda6e
DanYang <yangd@ge.com>
2016-06-01 13:24:50 -0400

    Modified to simplify depoloyment to Bamboo

--- CreateDB.bat
+++
@@ -1,5 +0,0 @@
-dropdb -U postgres gforge 
-psql -U postgres -c "CREATE DATABASE gforge WITH OWNER gforge;"
-psql -U postgres -d gforge < gforge.psql
-psql -U postgres -d gforge < insert_sample_data.psql
-psql -U postgres -d gforge < insert_servicerun_sample_data.psql
