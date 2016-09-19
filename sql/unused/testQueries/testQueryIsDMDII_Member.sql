756415b02a43a5dc58ecbb27746cf8d4fc64b1ca
dmcmillen <dmcmillen@nexient.com>
2016-09-19 13:33:04 -0400

    Cleaned up file directories, updated conventions.
    - Removed old files that will no longer be used.
    - Refactored core functionality and pre-loaded data.
    - Added support to run different version of data dependent on environment.
    
    We will now have two metadata tables - schema_version_core & schema_version_data.

--- sql/unused/testQueries/testQueryIsDMDII_Member.sql
+++
@@ -1,6 +0,0 @@
-﻿select * from organization_dmdii_member dmdii, organization_user orgu, users 
-where 
-users.user_name = 'joeengineer'
-and users.user_id=orgu.user_id 
-and orgu.organization_id = dmdii.organization_id 
-and dmdii.expire_date >= now() 
